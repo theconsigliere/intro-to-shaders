1d51d7e6f3e07c422df4122a14f912033be2f6e9
Dirty Martini <dirtymartini@Maxwells-MacBook-Pro-2.local>
2023-12-20 12:45:50 +0000

    4/6 started

--- src/sketches/003-step-smoothstep.glsl
+++ src/sketches/003-step-smoothstep.glsl
@@ -3,8 +3,22 @@ precision highp float;
 varying vec2 vUv;
 uniform float uTime;
 
+
+float normalSin(float val){
+  return sin(val) * 0.5 + 0.5;
+}
+
+float gradient (float attr) {
+// To avoid this range issue, we have to fix the range of the sine wave. Instead of -1 to 1, we'll make it 0 to 1:
+ float edge = normalSin(uTime); 
+
+ return step(edge, attr);
+}
+
 void main(){
   vec3 color = vec3(1.);
+
+  color = vec3 (gradient(vUv.x), 0., 0.);
 
   gl_FragColor = vec4(color, 1.0);
 }
